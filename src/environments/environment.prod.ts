7d2a2a6a706c554e70649dacbc97f38e0cb7b8db
Amit Shukla <ashuklax@gmail.com>
2018-06-18 22:28:57 -0700

    removed test settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
-    apiKey: "AIzaSyDgWsd3iLBKcE6fvKkLRTyxot6Ti-zlVfA",
-    authDomain: "mnjic9.firebaseapp.com",
-    databaseURL: "https://mnjic9.firebaseio.com",
-    projectId: "mnjic9",
-    storageBucket: "mnjic9.appspot.com",
-    messagingSenderId: "333638881937"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
